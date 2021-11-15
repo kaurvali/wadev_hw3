f7b163fff28e8c5f9bfa5ed7d48233f75ea93909
kaurvali <hullumeelne.vali@gmail.com>
2021-11-15 20:40:10 +0200

    Add about page content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About us!</h1>
+    <p>The great team of postIt developers behind this fun page:</p>
+    <ul>
+      <li>Kaur Vali (C05051)</li>
+      <li>Markus Sulg (B59673)</li>
+      <li>Lisa-Batricia Karu (B97060)</li>
+    </ul>
   </div>
 </template>
+
+<style scoped lang="scss">
+h1 {
+	margin-bottom: 25px;
+}
+p, ul {
+  font-size: 16pt;
+}
+ul {
+	list-style: none;
+}
+
+</style>
