2f40f16161f5e7fa005a67a393be4f21bb26f0fb
kaurvali <hullumeelne.vali@gmail.com>
2021-11-19 15:25:55 +0200

    Add reset likes button css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,54 @@
 <template>
   <div class="home">
-		<Post
-			v-for="post in posts"
-			:key="post.id"
-			:date="post.date"
-			:text="post.text"
-			:img="post.img"
-			:likes="post.likes"
-		/>
-		<button v-on:click="ResetLikes">Reset likes</button>
-	</div>
+    <Post
+      v-for="post in posts"
+      :key="post.id"
+      :date="post.date"
+      :text="post.text"
+      :img="post.img"
+      :likes="post.likes"
+    />
+    <button v-on:click="ResetLikes">Reset likes</button>
+  </div>
 </template>
 
 <script>
 import Post from "@/components/Post.vue";
 export default {
-	name: "Home",
-	components: {
-		Post,
-	},
-	computed: {
-		posts() {
-			return this.$store.state.posts;
-		},
-	},
-	methods: {
-		ResetLikes: function() {
-			this.$store.commit("ResetLikes")
-			//this.$store.state.posts.forEach(element => {
-			//	element.likes = 0;
-			//});
-		},
-	},
+  name: "Home",
+  components: {
+    Post,
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    },
+  },
+  methods: {
+    ResetLikes: function () {
+      this.$store.commit("ResetLikes");
+      //this.$store.state.posts.forEach(element => {
+      //	element.likes = 0;
+      //});
+    },
+  },
 };
 </script>
+
+<style scoped lang="scss">
+button {
+  background: black;
+  color: white;
+  border: none;
+  height: 35px;
+  width: 100px;
+  font-size: 12pt;
+  margin: 10px;
+}
+
+button:hover {
+  color: black;
+  background: white;
+  border: 1px solid black;
+}
+</style>
