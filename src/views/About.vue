72b7b03ad5f2d3fc4e482a592dda60dbca89c0b3
Lisa-Batricia <75910438+lisabatricia@users.noreply.github.com>
2021-11-21 22:45:01 +0200

    Added styling to contact and abou us page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,25 +2,63 @@
   <div class="about">
     <h1>About us!</h1>
     <p>The great team of postIt developers behind this fun page:</p>
-    <ul>
-      <li>Kaur Vali (C05051)</li>
-      <li>Markus Sulg (B59673)</li>
-      <li>Lisa-Batricia Karu (B97060)</li>
-    </ul>
-  </div>
+     <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Matriculum</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in users" :key="user.id">
+                    <td>{{user.name}}</td>
+                    <td>{{user.email}}</td>
+                    <td>{{user.id}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>    
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            users: [
+                { name: 'Kaur Vali', email: 'kaur.vali@postit.com', id: 'C05051' },
+                { name: 'Markus Sulg', email: 'markus.sulg@postit.com', id: 'B59673' },
+                { name: 'Lisa-Batricia Karu', email: 'lisabatricia.karu@postit.com', id: 'B97060' }
+            ]
+        };
+    }
+};
+</script>
+
+
 
 <style scoped lang="scss">
 h1 {
   padding-top: 5%;
 	margin-bottom: 25px;
 }
-p, ul {
-  font-size: 16pt;
+p {
+  color: #666;
+  font-weight: 400;
+  font-size: 18px;
   line-height: 30px;
 }
-ul {
-	list-style: none;
+table {
+  
+	background-color: lightgray;
+  margin: 10%;
 }
-
+td {
+  padding: 10px;
+}
+table{
+  border: 2px solid black;
+}
+  
 </style>
+
--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="contact-us">
     <h1>Contact us!</h1>
-    <p>
+    <p1>
       Got a question or idea?
       <br>
       We'd love to hear from you.
-      </p>
-    <ul>
-      <li>Phone: 555 555 55 </li>
-      <li>E-mail: <a href="mailto:info.postit@gmail.com">info.postit@gmail.com</a></li>
-    </ul>
+      </p1>
+    <p>
+    Phone: 555 555 55 
+    <br>
+    E-mail: <a href="mailto:info@postit.com">info@postit.com</a>
+    </p>
   </div>
 </template>
 
@@ -18,18 +19,14 @@ h1 {
   padding-top: 5%;
 	margin-bottom: 25px;
 }
-p {
+p1 {
   color: #666;
-  font-weight: 400;
-  font-size: 16px;
-  }
-p, ul {
-  font-size: 16pt;
-  line-height: 30px;
 }
-ul {
-  padding-top: 10%;
-	list-style: none;
+p1,p{
+ font-weight: 400;
+  font-size: 18px;
+  line-height: 30px;
+  margin-top: 10%;
 }
 
 </style>
